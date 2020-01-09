2a040cbdd54d7a9808f29450d53eb7a3f9800869
zackdk <958059970@qq.com>
2020-01-09 13:17:34 +0800

    step1-7

--- src/index.js
+++ src/index.js
@@ -35,7 +35,6 @@ function createDom(fiber) {
     fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
       : document.createElement(fiber.type);
-  const isProperty = key => key !== "children";
   updateDom(dom, {}, fiber.props)
 
   return dom;
@@ -62,16 +61,29 @@ function commitRoot() {
 
 function commitWork(fiber) {
   if (!fiber) return;
-  const domParent = fiber.parent.dom;
+  let domParentFiber = fiber.parent;
+  while(!domParentFiber.dom){
+    domParentFiber = domParentFiber.parent;
+  }
+  const domParent = domParentFiber.dom;
+
   if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
     domParent.appendChild(fiber.dom);
   } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
     updateDom(fiber.dom, fiber.alternate.props, fiber.props);
-  } else if (fiber.effectTag === "PLACEMENT") {
-    domParent.removeChild(fiber.dom);
+  } else if (fiber.effectTag === "DELETION") {
+    commitDeletion(fiber,domParent);
   }
   commitWork(fiber.child);
   commitWork(fiber.sibling);
+}
+
+function commitDeletion(fiber,domParent){
+  if(fiber.dom){
+    domParent.removeChild(fiber.dom);
+  }else{
+    commitDeletion(fiber.child,domParent);
+  }
 }
 
 function updateDom(dom, prevProps, nextProps) {
@@ -126,11 +138,13 @@ function workLoop(deadLine) {
 requestIdleCallback(workLoop);
 
 function performUnitOfWork(fiber) {
-  if (!fiber.dom) {
-    fiber.dom = createDom(fiber);
+
+  const isFunctionComponent = fiber.type instanceof Function;
+  if(isFunctionComponent){
+    updateFunctionComponent(fiber);
+  }else{
+    updateHostComponent(fiber);
   }
-  const elements = fiber.props.children;
-  reconcileChildren(fiber, elements)
   if (fiber.child) {
     return fiber.child;
   }
@@ -141,6 +155,19 @@ function performUnitOfWork(fiber) {
     }
     nextFiber = nextFiber.parent;
   }
+}
+
+function updateFunctionComponent(fiber){
+  const children = [fiber.type(fiber.props)];
+  reconcileChildren(fiber, children);
+}
+
+function updateHostComponent(fiber){
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  const elements = fiber.props.children;
+  reconcileChildren(fiber, elements);
 }
 
 function reconcileChildren(wipFiber, elements) {
@@ -201,20 +228,9 @@ const listener = ()=>{
   console.log("clicked!");
 }
 
-const element = (
-  <div id="foo">
-    <div>left</div>
-    <span classname="test">
-      <label>2222</label>
-      <a>2222</a>
-    </span>
-    <span classname="test">
-      <label>2222</label>
-      <a>2222</a>
-    </span>
-    <button onClick={listener} >test</button>
-  </div>
-);
-
-const container = document.getElementById("root");
-Didact.render(element, container);
+function App(props) {
+  return <h1>Hi {props.name}</h1>
+}
+const element = <App name="foo" />
+const container = document.getElementById("root")
+Didact.render(element, container)
